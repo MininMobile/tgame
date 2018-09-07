999e7f139c094330c59d036ca37415cac3b41d26
MininMobile <notminin@pm.me>
2018-09-07 08:21:31 +0100

    doing some things

---
+++ tgame/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tgame {
+	public class Game {
+		List<List<Char>> display = new List<List<Char>>() { };
+
+		public int Width { get; }
+		public int Height { get; }
+
+		public Game(int width = 128, int height = 32) {
+			Width = width;
+			Height = height;
+			Console.SetWindowSize(Width, Height);
+			Console.SetBufferSize(Width, Height);
+
+			// initialize display
+			for (int i = 0; i < Height; i++) {
+				display.Add(new List<Char>() { });
+				for (int j = 0; j < Width; j++) {
+					display[i].Add(Char.Empty);
+				}
+			}
+		}
+	}
+
+	enum Char {
+		Empty,
+		Wall,
+		Player,
+		Lazer,
+	}
+}
--- tgame/Program.cs
+++ tgame/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace tgame {
 	class Program {
 		static void Main(string[] args) {
+			Game game = new Game();
+			Console.ReadKey();
 		}
 	}
 }
